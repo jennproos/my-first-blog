54fb44ad18f2640591eb987dfdec6b0466a77611
Jenn Proos <jennproos@gmail.com>
2019-04-13 12:18:20 -0400

    Remove debug statements

--- blog/views.py
+++ blog/views.py
@@ -14,13 +14,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    print(request)
-    print(request.POST)
-    print(request.GET)
-    if "cancel" in request.POST:
-        print('here')
-        return redirect('blog/post_list.html')
-    elif request.method == "POST":
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
